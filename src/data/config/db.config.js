de43269e8f367c967461f902f6d18b087b841186
Patrick Richardson Wright <wrightmight@gmail.com>
2022-08-13 11:27:29 -0700

    update config

---
+++ src/data/config/db.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DBNAME_DEV,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  },
+  test: {
+    username: null,
+    password: null,
+    database: process.env.DBNAME_TEST,
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+  },
+};
