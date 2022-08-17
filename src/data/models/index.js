fce0975f70d30ac6cf75616f878575319c0e8ed3
Patrick Richardson Wright <wrightmight@gmail.com>
2022-08-16 22:38:03 -0700

    add back heroku sequelize connection code

--- src/data/models/index.js
+++ src/data/models/index.js
@@ -9,12 +9,12 @@ const config = require(__dirname + '/../config/db.config.js')[env];
 const db = {};
 
 let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(
-//     process.env[config.use_env_variable],
-//     config
-//   );
-// } else {
+if (config.use_env_variable) {
+  sequelize = new Sequelize(
+    process.env[config.use_env_variable],
+    config
+  );
+} else {
 sequelize = new Sequelize(
   config.database,
   config.username,
