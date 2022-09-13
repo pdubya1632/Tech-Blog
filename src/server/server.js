5108af5ad3d5933ec5e89580c6d2774a59e2e369
Patrick Richardson Wright <wrightmight@gmail.com>
2022-09-13 15:20:14 -0700

    add local tailwind css

--- src/server/server.js
+++ src/server/server.js
@@ -17,6 +17,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use('/public', express.static('public'));
 app.set('view engine', 'ejs');
 
 // redis config
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{ejs,html,js}', './views/**/*.{ejs,html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
